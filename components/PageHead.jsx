4ed4617933dd4971b650462a8d19c12162de2b13
Shivank Kacker <kacker.shivank@gmail.com>
2022-05-29 11:34:05 +0530

    UI improvements (#12)

---
+++ components/PageHead.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+export default function PageHead({title}){
+
+    return (
+        <Head>
+            <title>{title && title+" | "}Coronasafe Contributors</title>
+            <meta name="description" content="Coronasafe Leaderboard tracks the weekly progress of all coronasafe contributors." />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+    
+}

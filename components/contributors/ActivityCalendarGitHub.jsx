58b280c8577cedf3067c31b34d1c166a54a4ec55
Gokulram <gokulram2010418@ssn.edu.in>
2022-06-19 20:54:18 +0530

    moved the years section to the right side

--- components/contributors/ActivityCalendarGitHub.jsx
+++ components/contributors/ActivityCalendarGitHub.jsx
@@ -39,54 +39,80 @@ export default function ActivityCalenderGit({ calendarData }) {
     return dates;
   };
 
-  const lastFiveYears = (() => {
+  const lastNYears = (n) => {
     const currentYear = new Date().getFullYear();
     let years = [];
-    for (let i = 0; i < 5; i++) years.push(currentYear - i);
+    for (let i = 0; i < n; i++) years.push(currentYear - i);
     return years;
-  })();
+  };
 
-  const [year, setYear] = useState(lastFiveYears[0]);
+  const yearsList = lastNYears(4);
+
+  const [year, setYear] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
   const [activityData, setActivityData] = useState({});
 
   return (
-    <>
-      <div className="p-2 py-8 bg-gray-800 text-gray-100 text-center rounded-lg px-6 sm:px-10 xl:text-left mt-4">
-        <ActivityCalendar
-          showWeekdayLabels
-          data={getCalendarData(year)}
-          theme={{
-            level0: '#3A3B41',
-            level1: '#104D2F',
-            level2: '#027E3B',
-            level3: '#28AF44',
-            level4: '#39d353',
-            level5: '#50f26b',
-            level6: '#82F595',
-          }}
-          eventHandlers={{
-            onClick: (event) => (data) => {
-              setIsOpen(true);
-              setActivityData(data);
-            },
-          }}
-        ></ActivityCalendar>
+    <div className="flex gap-3">
+      <div className=" p-2 py-8 bg-gray-800 text-gray-100 text-center rounded-lg px-6 sm:px-10 xl:text-left mt-4">
+        {year === 0 ? (
+          <ActivityCalendar
+            showWeekdayLabels
+            data={calendarData}
+            theme={{
+              level0: '#3A3B41',
+              level1: '#104D2F',
+              level2: '#027E3B',
+              level3: '#28AF44',
+              level4: '#39d353',
+              level5: '#50f26b',
+              level6: '#82F595',
+            }}
+            eventHandlers={{
+              onClick: (event) => (data) => {
+                setIsOpen(true);
+                setActivityData(data);
+              },
+            }}
+            labels={{ totalCount: '{{count}} contributions in the last year' }}
+          ></ActivityCalendar>
+        ) : (
+          <ActivityCalendar
+            showWeekdayLabels
+            data={getCalendarData(year)}
+            theme={{
+              level0: '#3A3B41',
+              level1: '#104D2F',
+              level2: '#027E3B',
+              level3: '#28AF44',
+              level4: '#39d353',
+              level5: '#50f26b',
+              level6: '#82F595',
+            }}
+            eventHandlers={{
+              onClick: (event) => (data) => {
+                setIsOpen(true);
+                setActivityData(data);
+              },
+            }}
+          ></ActivityCalendar>
+        )}
+
         <ActivityModal
           isopen={isOpen}
           activityData={activityData}
           closeFunc={() => setIsOpen(false)}
         />
       </div>
-      <div className="grid grid-cols-5 gap-2 mt-2">
-        {lastFiveYears.map((y, i) => {
+      <div className="grid grid-cols-1 gap-2 mt-2">
+        {yearsList.map((y, i) => {
           return (
             <button
               key={i}
               className={
                 y !== year
-                  ? 'h-12 rounded-lg bg-gray-800 text-gray-100 flex justify-center items-center hover:bg-gray-700'
-                  : 'h-12 rounded-lg bg-green-600 text-white flex justify-center items-center'
+                  ? 'h-10 w-24 rounded-lg bg-gray-800 text-gray-100 flex text-sm justify-center items-center hover:bg-gray-700'
+                  : 'h-10 w-24 rounded-lg bg-green-600 text-white flex text-sm justify-center items-center'
               }
               onClick={(_) => setYear(y)}
             >
@@ -95,6 +121,6 @@ export default function ActivityCalenderGit({ calendarData }) {
           );
         })}
       </div>
-    </>
+    </div>
   );
 }

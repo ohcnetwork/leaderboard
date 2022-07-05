b75b72d0ff815ec046a3f961e3ebda39083f87ba
vinu tv <vinutv91@gmail.com>
2022-07-05 18:09:05 +0530

    Update styling on journey map for small screen

--- pages/contributors/[slug].js
+++ pages/contributors/[slug].js
@@ -48,9 +48,7 @@ export default function Contributor({ contributor, slug }) {
             <div className="pr-2 pb-2">
               <div>
                 <div className="flex items-center justify-center p-3 bg-gray-700 rounded-t-lg">
-                  <p className="text-white font-medium">
-                    Professional Skills - Self
-                  </p>
+                  <p className="text-white font-medium">Individual Skills</p>
                 </div>
                 <div className="grid grid-cols-6 divide-x divide-gray-600 border border-gray-600 bg-gray-800">
                   <div className="p-2"></div>
@@ -70,9 +68,7 @@ export default function Contributor({ contributor, slug }) {
             <div className="pl-2 pb-2 border-l-4 border-indigo-700">
               <div>
                 <div className="flex items-center justify-center p-3 bg-gray-700 rounded-t-lg">
-                  <p className="text-white font-medium">
-                    Professional Skills - Team
-                  </p>
+                  <p className="text-white font-medium">Team Skills</p>
                 </div>
                 <div className="grid grid-cols-6 divide-x divide-gray-600 border border-gray-600 bg-gray-800">
                   {professionalTeamSkills.map((skill) => (
@@ -122,7 +118,7 @@ export default function Contributor({ contributor, slug }) {
                   <div className="p-2"></div>
                 </div>
                 <div className="flex items-center justify-center p-3 bg-gray-700 rounded-b-lg">
-                  <p className="text-white font-medium">Human Values</p>
+                  <p className="text-white font-medium">Cultural Skills</p>
                 </div>
               </div>
             </div>
@@ -173,7 +169,7 @@ export default function Contributor({ contributor, slug }) {
                   <div className="md:pr-2 pb-2 bg-gray-800 flex flex-col md:justify-between flex-shrink-0 w-3/4 md:w-auto">
                     <div className="flex items-center md:justify-center p-3 bg-gray-700 rounded-t-lg">
                       <p className="text-white md:text-lg font-semibold">
-                        Professional Skills - Self
+                        Individual Skills
                       </p>
                     </div>
                     <div className="flex flex-wrap md:flex-row-reverse py-2 pl-2 gap-2">
@@ -190,7 +186,7 @@ export default function Contributor({ contributor, slug }) {
                   <div className="md:pl-2 pb-2 md:border-l-4 md:border-indigo-700 bg-gray-800 flex flex-col md:justify-between flex-shrink-0 w-3/4 md:w-auto">
                     <div className="flex items-center md:justify-center p-3 bg-gray-700 rounded-t-lg">
                       <p className="text-white md:text-lg font-semibold">
-                        Professional Skills - Team
+                        Team Skills
                       </p>
                     </div>
                     <div className="flex flex-wrap py-2 pl-2 md:pl-0 md:pr-2 gap-2">
@@ -234,12 +230,66 @@ export default function Contributor({ contributor, slug }) {
                     </div>
                     <div className="flex items-center md:justify-center p-3 bg-gray-700 rounded-t-lg md:rounded-b-lg">
                       <p className="text-white md:text-lg font-semibold">
-                        Human Values
+                        Cultural Skills
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
+
+            <div className="">
+              {contributor.courses_completed && (
+                <div>
+                  <h3 className="font-bold text-gray-100 mt-4">Journey Map</h3>
+                  <div className="journey-map__container relative bg-gray-800 text-center rounded-lg px-4 py-8 sm:px-8 xl:text-left mt-4">
+                    <div className="flex flex-col md:flex-row overflow-x-auto md:space-y-0 md:space-x-4 font-semibold">
+                      {contributor.courses_completed.map((course) => (
+                        <div
+                          key={course}
+                          className="relative flex flex-col items-center justify-center"
+                        >
+                          <div className="journey-map__milestone relative z-10 p-2 w-24 h-24 bg-gray-600 rounded-full flex items-center text-center shadow-xl"></div>
+                          <p className="text-sm text-center text-gray-300 mt-2 z-10 bg-gray-800 py-0.5">
+                            {course}
+                          </p>
+                          <div className="absolute top-12 z-20">
+                            <img
+                              className="mx-auto h-9 w-9"
+                              src="/images/map-check.png"
+                            ></img>
+                          </div>
+                        </div>
+                      ))}
+
+                      <div className="relative  flex flex-col items-center juustify-center">
+                        <div className="relative journey-map__milestone z-10 p-2 w-24 h-24 bg-gray-600 rounded-full flex items-center text-center shadow-xl"></div>
+                        <p className="text-sm text-center text-gray-300 mt-2 z-10 bg-gray-800 py-0.5">
+                          Teaching Assistant &amp; Internship
+                        </p>
+                        <div className="absolute top-8 md:top-5 animate-bounce z-20 flex items-center justify-center">
+                          <img
+                            className="mx-auto w-14 md:w-16"
+                            src="/images/map-pointer.png"
+                          ></img>
+                          <img
+                            className="absolute top-1.5 mx-auto h-7 md:h-8 w-7 md:w-8 rounded-full border border-primary-200"
+                            src={`https://github.com/${contributor.github}.png`}
+                            alt={contributor.github}
+                          />
+                        </div>
+                      </div>
+                      <div className="relative flex flex-col items-center juustify-center">
+                        <div className="relative journey-map__milestone z-10 p-2 w-24 h-24 bg-gray-600 rounded-full flex items-center text-center shadow-xl"></div>
+                        <p className="text-sm text-center text-gray-300 mt-2 z-10 bg-gray-800 py-0.5">
+                          Industry Placement
+                        </p>
+                      </div>
+                    </div>
+                    <div className="w-2.5 md:w-auto md:h-2.5 absolute bg-gray-900 top-24 bottom-20 left-1/2 transform -translate-x-1/2 md:-translate-x-0 md:left-20 md:right-20 z-1 md:bottom-24 mt-5"></div>
+                  </div>
+                </div>
+              )}
             </div>
 
             <div>
@@ -268,55 +318,6 @@ export default function Contributor({ contributor, slug }) {
                   data={contributor.calendarData}
                 />
               </div>
-            </div>
-            <div className="">
-              {contributor.courses_completed && (
-                <div>
-                  <h3 className="font-bold text-gray-100 mt-4">Journey Map</h3>
-                  <div className="journey-map__container relative p-2 py-8 bg-gray-800 text-center rounded-lg px-6 sm:px-10 xl:text-left mt-4">
-                    <div className="flex overflow-x-auto space-x-8 font-semibold">
-                      {contributor.courses_completed.map((course) => (
-                        <div
-                          key={course}
-                          className="flex flex-col items-center juustify-center"
-                        >
-                          <div className="journey-map__milestone relative z-10 p-2 w-24 h-24 bg-gray-600 rounded-full flex items-center text-center shadow-xl"></div>
-                          <p className="text-center text-gray-300 -mt-4">
-                            {course}
-                          </p>
-                          <div className="absolute top-12 z-20">
-                            <img
-                              className="mx-auto h-9 w-9"
-                              src="/images/map-check.png"
-                            ></img>
-                          </div>
-                        </div>
-                      ))}
-
-                      <div className="flex flex-col items-center juustify-center">
-                        <div className="relative journey-map__milestone z-10 p-2 w-24 h-24 bg-gray-600 rounded-full flex items-center text-center shadow-xl"></div>
-                        <p className="text-center text-gray-300 -mt-4">
-                          Industry Internship
-                        </p>
-                        <div className="absolute top-8 animate-bounce ">
-                          <img
-                            className="mx-auto h-8 w-8 rounded-full border border-indigo-500"
-                            src={`https://github.com/${contributor.github}.png`}
-                            alt={contributor.github}
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-col items-center juustify-center">
-                        <div className="journey-map__milestone z-10 p-2 w-24 h-24 bg-gray-600 rounded-full flex items-center text-center shadow-xl"></div>
-                        <p className="text-center text-gray-300 -mt-4">
-                          Industry Placement
-                        </p>
-                      </div>
-                    </div>
-                    <div className="h-1.5 absolute bg-primary-400 left-20 right-20 z-1 bottom-24 mb-2"></div>
-                  </div>
-                </div>
-              )}
             </div>
             <div>
               <h3 className="font-bold text-gray-100 mt-6">Highlights</h3>

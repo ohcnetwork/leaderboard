09a64588d308d12dc89c1cbd3f509121202eda67
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-11-02 17:17:50 +0530

    og:url meta tag

--- components/PageHead.jsx
+++ components/PageHead.jsx
@@ -4,6 +4,10 @@ export default function PageHead({ title, metaTags }) {
     <Head>
       <title>{title && title + " | "}Coronasafe Contributors</title>
       <meta
+        property="og:url"
+        content={"https://contributors.coronasafe.network/"}
+      />
+      <meta
         name="description"
         content="Coronasafe Leaderboard tracks the weekly progress of all coronasafe contributors."
       />

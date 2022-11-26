1bc8ab36707dd6715737453c78f15df87f503bf1
Ritesh Aggarwal <riteshaggarwal2001@gmail.com>
2022-11-26 17:29:53 +0530

    Preview images for links (#100)

--- components/PageHead.jsx
+++ components/PageHead.jsx
@@ -1,12 +1,21 @@
 import Head from "next/head";
-export default function PageHead({title}){
-
-    return (
-        <Head>
-            <title>{title && title+" | "}Coronasafe Contributors</title>
-            <meta name="description" content="Coronasafe Leaderboard tracks the weekly progress of all coronasafe contributors." />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    )
-    
+export default function PageHead({ title, metaTags }) {
+  return (
+    <Head>
+      <title>{title && title + " | "}Coronasafe Contributors</title>
+      <meta
+        property="og:url"
+        content={"https://contributors.coronasafe.network"}
+      />
+      <meta
+        name="description"
+        content="Coronasafe Leaderboard tracks the weekly progress of all coronasafe contributors."
+      />
+      {metaTags &&
+        metaTags.map((tag) => (
+          <meta key={tag.name} property={tag.name} content={tag.content} />
+        ))}
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
 }

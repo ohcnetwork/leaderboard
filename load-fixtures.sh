833bb5e1a802e4d9cd96a84e4a15f63c8f67afa5
rithviknishad <mail@rithviknishad.dev>
2023-12-11 16:14:58 +0530

    Add symlinks and support to load fixtures

---
+++ load-fixtures.sh
@@ -0,0 +1,13 @@
+: "${FIXTURES_SOURCE:=https://github.com/coronasafe/leaderboard-data.git}"
+
+mkdir -p data-repo
+cd data-repo
+
+git init
+git remote add --mirror=fetch origin ${FIXTURES_SOURCE}
+git config core.sparseCheckout true
+
+echo "data/" >> .git/info/sparse-checkout
+echo "contributors/" >> .git/info/sparse-checkout
+
+git pull origin main

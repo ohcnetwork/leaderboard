3a5ddc2ab1f02ff8bc13b2ceeae1b0576a9ee211
rithviknishad <mail@rithviknishad.dev>
2024-02-12 16:40:22 +0530

    fix api route

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -89,5 +89,7 @@ export async function GET(request: Request) {
   const dateRange = parseDateRangeSearchParam(searchParams.get("between"));
   const ordering = searchParams.get("sort") ?? "-points";
 
-  return Response.json(getLeaderboardData(dateRange, ordering as OrderingKey));
+  const data = await getLeaderboardData(dateRange, ordering as OrderingKey);
+
+  return Response.json(data);
 }

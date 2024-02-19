7a02d6cfaf10ac607b0d2f1a3c2f37b7c8455189
rithviknishad <mail@rithviknishad.dev>
2024-02-19 21:09:26 +0530

    Adds `/people` gallery view

---
+++ app/people/page.tsx
@@ -0,0 +1,28 @@
+import { getContributors } from "@/lib/api";
+import Link from "next/link";
+
+export default async function Page() {
+  const contributors = (await getContributors()).sort(
+    (a, b) => b.highlights.points - a.highlights.points,
+  );
+
+  return (
+    <ul className="flex flex-wrap gap-2 max-w-7xl mx-auto my-8 relative justify-center">
+      {contributors.map((c) => (
+        <li
+          key={c.github}
+          className="hover:scale-125 transition-all duration-150 ease-in-out shadow-sm shadow-primary-500 hover:shadow-xl hover:shadow-primary-500"
+        >
+          <Link href={`/contributors/${c.github}`}>
+            <img
+              className="rounded-lg h-16 w-16 hover:ring hover:ring-primary-500"
+              src={`https://avatars.githubusercontent.com/${c.github}`}
+              alt={c.github}
+              title={`${c.name} - @${c.github}`}
+            />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}

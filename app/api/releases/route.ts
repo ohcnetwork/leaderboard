29229278098726d19c0ff2f66e7cf13314fba04b
kirtanchandak <themarketingkirtan@gmail.com>
2024-02-24 12:24:59 +0530

    releases fetched from graphql

---
+++ app/api/releases/route.ts
@@ -0,0 +1,80 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { GraphQLClient } from "graphql-request";
+
+interface Release {
+    name: string;
+    createdAt: string;
+    description: string;
+    url: string;
+}
+
+interface Repository {
+    name: string;
+    releases: {
+        nodes: Release[];
+    };
+}
+
+interface Organization {
+    repositories: {
+        nodes: Repository[];
+    };
+}
+
+interface GitHubResponse {
+    organization: Organization;
+}
+
+const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN as string;
+const NEXT_PUBLIC_GITHUB_ORG = process.env.NEXT_PUBLIC_GITHUB_ORG as string;
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    const query = `
+  query {
+    organization(login: "${NEXT_PUBLIC_GITHUB_ORG}") {
+      repositories(first: 100) {
+        nodes {
+          name
+          releases(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
+            nodes {
+              name
+              createdAt
+              description
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+  `;
+
+    try {
+        const client = new GraphQLClient('https://api.github.com/graphql', {
+            headers: {
+                Authorization: `Bearer ${GITHUB_API_TOKEN}`
+            }
+        });
+
+        const data: GitHubResponse = await client.request(query);
+
+        if (!data.organization) {
+            return NextResponse.json({ error: `Organization "${NEXT_PUBLIC_GITHUB_ORG}" not found` }, { status: 404 });
+        }
+
+        const repositories: Repository[] = data.organization.repositories.nodes;
+        const allReleases: Release[] = [];
+        for (const repository of repositories) {
+            allReleases.push(...repository.releases.nodes);
+        }
+
+
+        const sortedReleases = allReleases.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+        const latestReleases = sortedReleases.slice(0, 10);
+
+        return NextResponse.json(latestReleases);
+    } catch (error) {
+        console.error('Error fetching repositories and releases:', error);
+        return NextResponse.json({ error: 'Failed to fetch repositories and releases' }, { status: 500 });
+    }
+}

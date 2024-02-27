69f06d1f10315a5f934e6b3e3d0505dee39f8cf3
rithviknishad <mail@rithviknishad.dev>
2024-02-27 19:22:02 +0530

    fix lint

--- app/releases/page.tsx
+++ app/releases/page.tsx
@@ -6,9 +6,7 @@ export default function Page() {
   return (
     <div className="mx-auto max-w-4xl p-10">
       <h1 className="text-4xl pb-10">Recent Releases</h1>
-      <Suspense
-        fallback={<LoadingText text="Fetching latest releases" />}
-      >
+      <Suspense fallback={<LoadingText text="Fetching latest releases" />}>
         <Releases className="flex flex-col gap-10" />
       </Suspense>
     </div>

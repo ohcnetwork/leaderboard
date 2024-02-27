bcbd16d3632fb6d854865df0ea84203049a5d7e3
kirtanchandak <themarketingkirtan@gmail.com>
2024-02-27 23:04:24 +0530

    added layout for releases

--- app/releases/page.tsx
+++ app/releases/page.tsx
@@ -4,17 +4,14 @@ import LoadingText from "@/components/LoadingText";
 
 export default function Page() {
   return (
-    <div className="mx-auto max-w-4xl p-10">
-      <h1 className="text-4xl pb-10">Recent Releases</h1>
-      <Suspense
-        fallback={
-          <>
-            <LoadingText text="Fetching latest releases" />
-          </>
-        }
-      >
-        <Releases className="flex flex-col gap-10" />
-      </Suspense>
-    </div>
+    <Suspense
+      fallback={
+        <>
+          <LoadingText text="Fetching latest releases" />
+        </>
+      }
+    >
+      <Releases className="flex flex-col gap-10" />
+    </Suspense>
   );
 }

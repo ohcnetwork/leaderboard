bf021636f6647cb5cdc91bae2a1ec5edecd4786a
Rithvik Nishad <rithvikn2001@gmail.com>
2024-02-27 13:11:35 +0000

    Update app/releases/page.tsx

--- app/releases/page.tsx
+++ app/releases/page.tsx
@@ -7,11 +7,7 @@ export default function Page() {
     <div className="mx-auto max-w-4xl p-10">
       <h1 className="text-4xl pb-10">Recent Releases</h1>
       <Suspense
-        fallback={
-          <>
-            <LoadingText text="Fetching latest releases" />
-          </>
-        }
+        fallback={<LoadingText text="Fetching latest releases" />}
       >
         <Releases className="flex flex-col gap-10" />
       </Suspense>

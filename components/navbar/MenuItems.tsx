0c701f83dbeed240ab4c2078aab08375b3a7eafd
Chao Lobi <gogoiladitya@gmail.com>
2024-02-29 12:28:04 +0530

    Updating the gradient

--- components/navbar/MenuItems.tsx
+++ components/navbar/MenuItems.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const MenuItems = () => {
   return (
-    <div className="flex flex-row items-center justify-between rounded bg-gradient-to-r from-blue-500 to-purple-500 px-1 py-1 font-inter font-semibold md:rounded-full md:px-4 md:py-2">
+    <div className="flex flex-row items-center justify-between rounded bg-gray-100 px-1 py-1 font-inter font-semibold dark:bg-gray-700/50 md:rounded-full md:px-4 md:py-2">
       <Link href="/leaderboard">
         <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base">
           Leaderboard

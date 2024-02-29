59a0973bcc54a464af817cfe85ab549277219899
Chao Lobi <gogoiladitya@gmail.com>
2024-02-29 19:01:59 +0530

    updating navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -12,31 +12,33 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="sticky top-0 z-10 flex items-center justify-between px-4 py-1 font-inter backdrop-blur">
-        <Logo />
-        <div className="hidden lg:flex">
-          <MenuItems />
-        </div>
-        <div className="align-center flex flex-row justify-center gap-4">
-          <ContributeButton />
-          <ThemeSwitch />
-          <div
-            className="flex  cursor-pointer items-center justify-center transition-transform duration-300 ease-in-out lg:hidden"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? (
-              <IoClose className="text-3xl" />
-            ) : (
-              <RxHamburgerMenu className="text-3xl" />
-            )}
+      <div className="sticky top-0 z-10 px-4 py-1 font-inter backdrop-blur">
+        <div className="flex items-center justify-between">
+          <Logo />
+          <div className="hidden lg:flex">
+            <MenuItems />
+          </div>
+          <div className="align-center flex flex-row justify-center gap-4">
+            <ContributeButton />
+            <ThemeSwitch />
+            <div
+              className="flex  cursor-pointer items-center justify-center transition-transform duration-300 ease-in-out lg:hidden"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? (
+                <IoClose className="text-3xl" />
+              ) : (
+                <RxHamburgerMenu className="text-3xl" />
+              )}
+            </div>
           </div>
         </div>
+        {open && (
+          <div className="px-2 py-1 transition-transform duration-300 ease-in-out lg:hidden">
+            <MenuItems />
+          </div>
+        )}
       </div>
-      {open && (
-        <div className="px-2 py-1 transition-transform duration-300 ease-in-out lg:hidden">
-          <MenuItems />
-        </div>
-      )}
     </>
   );
 };

4a69c4d544dfb1145e49614dc5d3a415cfbd3024
Chao Lobi <gogoiladitya@gmail.com>
2024-03-02 01:23:55 +0530

    color is updated

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -10,17 +10,17 @@ export default function ThemeSwitch({ className }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center ${className}`}>
       <div
-        className={`flex cursor-pointer items-center justify-center gap-2 rounded-md bg-primary-100 p-1 dark:bg-primary-500`}
+        className={`flex cursor-pointer items-center justify-center gap-2 rounded-md bg-gray-100 p-1 dark:bg-gray-800/50`}
       >
         <div
           onClick={() => setTheme("light")}
-          className={`flex items-center justify-center rounded-md bg-primary-50 p-2 dark:bg-transparent`}
+          className={`flex items-center justify-center rounded-md bg-white p-2 dark:bg-transparent`}
         >
           <MdOutlineLightMode className="text-lg" />
         </div>
         <div
           onClick={() => setTheme("dark")}
-          className={`flex items-center justify-center rounded-md bg-transparent p-2 dark:bg-primary-700`}
+          className={`flex items-center justify-center rounded-md bg-transparent p-2 dark:bg-gray-700/50`}
         >
           <MdOutlineDarkMode className="text-lg" />
         </div>

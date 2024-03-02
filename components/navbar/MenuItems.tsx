3da78db58901a06da7b2c4b917c4bd3aae9e6c1a
Chao Lobi <gogoiladitya@gmail.com>
2024-03-02 11:32:22 +0530

    adding release section

--- components/navbar/MenuItems.tsx
+++ components/navbar/MenuItems.tsx
@@ -4,23 +4,28 @@ const MenuItems = () => {
   return (
     <div className="flex flex-row items-center justify-between rounded bg-gray-100 px-1 py-1 font-semibold dark:bg-gray-700/50 md:rounded-full md:px-4 md:py-2">
       <Link href="/leaderboard">
-        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base">
+        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base hover:underline hover:text-primary-500">
           Leaderboard
         </p>
       </Link>
       <Link href="/people">
-        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base">
+        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base hover:underline hover:text-primary-500">
           Contributors
         </p>
       </Link>
       <Link href="/projects">
-        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base">
+        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base hover:underline hover:text-primary-500">
           Projects
         </p>
       </Link>
       <Link href="/feed">
-        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base">
-          Feedback
+        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base hover:underline hover:text-primary-500">
+          Feed
+        </p>
+      </Link>
+      <Link href="/releases">
+        <p className="cursor-pointer p-1 text-sm md:p-2 md:text-base hover:underline hover:text-primary-500">
+          Releases
         </p>
       </Link>
     </div>

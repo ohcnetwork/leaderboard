0d5784d3ee09f03419316f3da7b946f7f35f5a90
rithviknishad <mail@rithviknishad.dev>
2024-03-04 13:54:33 +0530

    fix bg color in navbar

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -9,18 +9,16 @@ export default function ThemeSwitch({ className }: { className?: string }) {
 
   return (
     <div className={`flex items-center justify-center ${className}`}>
-      <div
-        className={`dark:bg-secondary-800/50 flex cursor-pointer items-center justify-center gap-2 rounded-md bg-secondary-100 p-1`}
-      >
+      <div className="flex cursor-pointer items-center justify-center gap-2 rounded-md bg-secondary-100 p-1 dark:bg-secondary-800">
         <div
           onClick={() => setTheme("light")}
-          className={`flex items-center justify-center rounded-md bg-white p-2 dark:bg-transparent`}
+          className="flex items-center justify-center rounded-md bg-white p-2 dark:bg-transparent"
         >
           <MdOutlineLightMode className="text-lg" />
         </div>
         <div
           onClick={() => setTheme("dark")}
-          className={`dark:bg-secondary-700/50 flex items-center justify-center rounded-md bg-transparent p-2`}
+          className="flex items-center justify-center rounded-md bg-transparent p-2 dark:bg-secondary-900"
         >
           <MdOutlineDarkMode className="text-lg" />
         </div>

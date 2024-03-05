c9076d550ed42883396ba6ba4e9ec51a71a4978e
rithviknishad <mail@rithviknishad.dev>
2024-03-05 15:53:04 +0530

    adds missing shebang to `load-org-data`

--- load-org-data.sh
+++ load-org-data.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 : "${FIXTURES_SOURCE:=https://github.com/coronasafe/leaderboard-data.git}"
 
 if [ -d "data-repo/.git" ] && (cd data-repo && git remote -v | grep -q 'upstream'); then

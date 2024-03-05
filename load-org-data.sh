f05502492d7d9da98cc688239f75d19ddcf10c8e
Uday Sagar <udaysagar.mail@gmail.com>
2024-03-05 14:52:23 +0530

    updated script

--- load-org-data.sh
+++ load-org-data.sh
@@ -1,16 +1,31 @@
 : "${FIXTURES_SOURCE:=https://github.com/coronasafe/leaderboard-data.git}"
 
-rm -rf data-repo
-mkdir data-repo
-cd data-repo
+if [ -d "data-repo/.git" ]; then
+  echo "Updating existing data repository..."
+  
+  cd data-repo
+  
+  git fetch upstream
+  git checkout --force main
+  git reset --hard main
+  
+  cd ..
+else
+  echo "Cloning data repository for the first time..."
 
-git init
-git remote add --mirror=fetch origin ${FIXTURES_SOURCE}
-git config core.sparseCheckout true
-
-echo "data/" >> .git/info/sparse-checkout
-echo "contributors/" >> .git/info/sparse-checkout
-echo "config/" >> .git/info/sparse-checkout
-
-git pull origin main
+  rm -rf data-repo
+  mkdir data-repo
+  cd data-repo
+  
+  git init
+  git remote add --mirror=fetch upstream ${FIXTURES_SOURCE}
+  git config core.sparseCheckout true
+  
+  echo "data/" >> .git/info/sparse-checkout
+  echo "contributors/" >> .git/info/sparse-checkout
+  echo "config/" >> .git/info/sparse-checkout
+  
+  git pull upstream main
+  cd ..
+fi
 

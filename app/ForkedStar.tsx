00a2d6c4f26daabb1c544d839aa1d00f7be0af05
Ashutosh Rai <ashu00jnp@gmail.com>
2024-03-07 02:42:46 +0530

    added forked and star count

--- app/ForkedStar.tsx
+++ app/ForkedStar.tsx
@@ -1,6 +1,12 @@
+// const { Octokit } = require("@octokit/action");
+const { Octokit } = require("octokit");
 import { env } from "@/env.mjs";
 import { GoRepoForked } from "react-icons/go";
 import { GoStar } from "react-icons/go";
+
+const octokit = new Octokit({
+  auth: env.GITHUB_TOKEN,
+});
 
 export default async function ForkedStar() {
   let forkCount = 0;
@@ -8,7 +14,7 @@ export default async function ForkedStar() {
 
   const data = await getData();
 
-  data?.map((ls: any) => {
+  data?.map((ls: { forks: number; stargazers_count: number }) => {
     if (ls.forks) {
       forkCount += ls.forks;
     }
@@ -21,18 +27,18 @@ export default async function ForkedStar() {
     <div className="flex justify-center">
       {forkCount > 0 && starCount > 0 ? (
         <>
-          <div className="flex items-center justify-center bg-primary-700 m-4 p-2 w-6/12 rounded-md border-2 border-gray-500 ">
-            <GoRepoForked className=" text-lg font-bold mr-1 text-gray-300" />
+          <div className="m-4 flex w-6/12 items-center justify-center rounded-md border-2 border-gray-500 bg-primary-700 p-2">
+            <GoRepoForked className="mr-1 text-lg font-bold" />
             <div className="text-gray-300">Forked</div>
-            <div className="bg-background py-2 px-4 rounded-md ml-4">
+            <div className="ml-4 rounded-md bg-background px-4 py-2">
               {forkCount}
             </div>
           </div>
-          <div className="flex items-center justify-center bg-primary-700 m-4 p-2 w-6/12 rounded-md border-2 border-gray-500">
-            <GoStar className=" text-lg font-bold mr-1 text-gray-300" />
+          <div className="m-4 flex w-6/12 items-center justify-center rounded-md border-2 border-gray-500 bg-primary-700 p-2">
+            <GoStar className="mr-1 text-lg font-bold" />
             <div className="text-gray-300">Starred</div>
 
-            <div className="bg-background py-2 px-4 rounded-md ml-4">
+            <div className="ml-4 rounded-md bg-background px-4 py-2">
               {starCount}
             </div>
           </div>
@@ -45,14 +51,13 @@ export default async function ForkedStar() {
 }
 
 async function getData() {
-  const res = await fetch(
-    `https://api.github.com/orgs/${env.NEXT_PUBLIC_GITHUB_ORG}/repos`,
+  const res = await octokit.paginate(
+    `GET /orgs/${env.NEXT_PUBLIC_GITHUB_ORG}/repos`,
+    {
+      org: env.NEXT_PUBLIC_GITHUB_ORG,
+      per_page: 1000,
+    },
   );
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
+  return res;
 }

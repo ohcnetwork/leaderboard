798ffb7c13b0deaf4b3d7fd6ff7188d5ba6c3b67
rash-27 <rashmik2705@gmail.com>
2024-03-08 17:43:42 +0530

    Add bio in hover card

--- app/people/_components/HoverInfoCard.tsx
+++ app/people/_components/HoverInfoCard.tsx
@@ -1,5 +1,6 @@
+import Markdown from "@/components/Markdown";
 import { Contributor } from "@/lib/types";
-import clsx from "clsx";
+
 import Image from "next/image";
 
 export default function InfoCard({
@@ -9,16 +10,11 @@ export default function InfoCard({
 }) {
   return (
     <div
-      className={clsx(
-        "w-44 rounded-lg bg-secondary-300 dark:bg-secondary-800 xl:text-left ",
-        "border-2 border-primary-700",
-      )}
+      className="max-h-64 w-44 overflow-hidden rounded-lg border-2 border-primary-700 bg-secondary-300 pb-2 dark:bg-secondary-800 xl:text-left"
       role="listitem"
     >
       <div className="flex shrink-0 items-center  md:space-y-2 xl:space-y-1">
-        <div
-          className={`z-10 flex h-16 w-16 shrink-0 items-center rounded-full md:h-20 md:w-20 md:p-1`}
-        >
+        <div className="z-10 flex h-16 w-16 shrink-0 items-center rounded-full md:h-20 md:w-20 md:p-1">
           <Image
             className="rounded-full border-2 border-indigo-500"
             src={`https://avatars.githubusercontent.com/${contributor.github}`}
@@ -29,11 +25,7 @@ export default function InfoCard({
         </div>
         <div className={`overflow-hidden `}>
           <div className="fnt-medium space-y-1 overflow-hidden text-sm">
-            <h3
-              className={clsx(
-                "md:text-md overflow-hidden text-ellipsis text-sm leading-tight",
-              )}
-            >
+            <h3 className="md:text-md overflow-hidden text-ellipsis text-base leading-tight">
               {contributor.name}
             </h3>
             <p className="text-xs text-secondary-600 dark:text-secondary-400 ">
@@ -42,6 +34,9 @@ export default function InfoCard({
           </div>
         </div>
       </div>
+      <p className="max-h-64 scale-75 text-xs text-secondary-700 dark:text-secondary-300 ">
+        <Markdown>{contributor.content}</Markdown>
+      </p>
     </div>
   );
 }

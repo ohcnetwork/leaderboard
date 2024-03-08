74ea68b8ceb7f084839faca38afd7e9e717ce3f9
Ashutosh Rai <ashu00jnp@gmail.com>
2024-03-08 15:32:21 +0530

    removed fetch

--- components/gh_events/GitHubEvents.tsx
+++ components/gh_events/GitHubEvents.tsx
@@ -1,8 +1,7 @@
-"use client";
 import { IGitHubEvent, combineSimilarPushEvents } from "@/lib/gh_events";
-import { useEffect, useState } from "react";
 import GitHubEvent from "./GitHubEvent";
 import { env } from "@/env.mjs";
+import octokit from "@/lib/octokit";
 
 const exludeBotEvents = (event: IGitHubEvent) => {
   return !event.actor.login.includes("bot");
@@ -21,29 +20,21 @@ const excludeBlacklistedEvents = (event: IGitHubEvent) => {
   return !blacklist.includes(event.type);
 };
 
-export default function GitHubEvents({ minimal }: { minimal?: boolean }) {
-  const [page, setPage] = useState(1);
-  const [events, setEvents] = useState<IGitHubEvent[]>();
-
-  useEffect(() => {
-    fetch(
-      `https://api.github.com/orgs/${env.NEXT_PUBLIC_GITHUB_ORG}/events?per_page=100&page=${page}`,
-    )
-      .then((res) => res.json())
-      .then((data) =>
-        setEvents(
-          combineSimilarPushEvents(
-            data.filter(exludeBotEvents).filter(excludeBlacklistedEvents),
-          ).slice(0, 5),
-        ),
-      );
-  }, [page]);
+export default async function GitHubEvents({ minimal }: { minimal?: boolean }) {
+  const events = await getEvent().then((data: any) =>
+    data.filter(excludeBlacklistedEvents).filter(exludeBotEvents).slice(0, 5),
+  );
+  if (!env.NEXT_PUBLIC_GITHUB_ORG) {
+    throw new Error("Missing NEXT_PUBLIC_GITHUB_ORG");
+  }
 
   return (
     <div className="flow-root">
       <ul role="list" className="-mb-8 mt-4 flex flex-col gap-4 space-y-4">
         {events ? (
-          events.map((e) => <GitHubEvent key={e.id} event={e} />)
+          events.map((e: any) => (
+            <GitHubEvent key={e.id} event={e as IGitHubEvent | undefined} />
+          ))
         ) : (
           <>
             <GitHubEvent />
@@ -56,4 +47,14 @@ export default function GitHubEvents({ minimal }: { minimal?: boolean }) {
       </ul>
     </div>
   );
+}
+
+async function getEvent() {
+  const response = await octokit.paginate("GET /orgs/{org}/events", {
+    org: env.NEXT_PUBLIC_GITHUB_ORG,
+    per_page: 100,
+    page: 1,
+  });
+
+  return response;
 }

70fb46c8c170c3253cbf63d78cc9f0bf1ae74708
rithviknishad <mail@rithviknishad.dev>
2024-03-11 11:50:12 +0530

    make code readable and fix light mode / dark mode

--- app/CommunityEngagementSummary.tsx
+++ app/CommunityEngagementSummary.tsx
@@ -5,71 +5,48 @@ import { GoStar } from "react-icons/go";
 import { GoPeople } from "react-icons/go";
 import { getContributorsSlugs } from "@/lib/api";
 
-const GITHUB_ORG: string = env.NEXT_PUBLIC_GITHUB_ORG;
-
 export default async function CommunityEngagemet() {
-  const count = await getData().then((data) => {
-    type ForkAndStar = {
-      forks: number;
-      stargazers_count: number;
-    };
+  const data = await octokit.paginate(
+    "GET /orgs/{org}/repos",
+    {
+      org: env.NEXT_PUBLIC_GITHUB_ORG,
+      per_page: 1000,
+      sort: "updated",
+      direction: "desc",
+    },
+    (res) =>
+      res.data.map((obj) => ({
+        forks: obj.forks ?? 0,
+        stars: obj.stargazers_count ?? 0,
+      })),
+  );
 
-    return data.reduce(
-      (acc: ForkAndStar, ls) => {
-        return {
-          forks: acc.forks + ls.forks,
-          stargazers_count: acc.stargazers_count + ls.stargazers_count,
-        };
-      },
-      { forks: 0, stargazers_count: 0 },
-    );
-  });
+  const { forks, stars } = data.reduce(
+    (acc, obj) => ({
+      forks: acc.forks + obj.forks,
+      stars: acc.stars + obj.stars,
+    }),
+    { forks: 0, stars: 0 },
+  );
 
   const contributors = await getContributorsSlugs();
 
   return (
-    <div className="flex justify-center">
-      <>
-        <div className="g-secondary-100 m-4 flex w-4/12 items-center justify-center rounded-md border-2 border-secondary-700 p-2 dark:bg-secondary-800">
-          <GoRepoForked className="mr-2 text-lg font-bold text-secondary-200 " />
-          <div className="tracking-wider/widest font-bold">{count.forks}</div>
-        </div>
-        <div className="g-secondary-100 m-4 flex w-4/12 items-center justify-center rounded-md border-2 border-secondary-700 p-2 dark:bg-secondary-800">
-          <GoStar className="mr-2 text-lg font-bold text-secondary-200" />
+    <div className="flex justify-between">
+      <div className="m-4 flex w-4/12 items-center justify-center rounded-md border border-secondary-200 bg-secondary-100 p-2 dark:border-secondary-700 dark:bg-secondary-800">
+        <GoRepoForked className="mr-2 text-lg font-bold text-foreground" />
+        <div className="font-bold tracking-wider">{forks}</div>
+      </div>
+      <div className="m-4 flex w-4/12 items-center justify-center rounded-md border border-secondary-200 bg-secondary-100 p-2 dark:border-secondary-700 dark:bg-secondary-800">
+        <GoStar className="mr-2 text-lg font-bold text-foreground" />
 
-          <div className="tracking-wider/widest font-bold">
-            {count.stargazers_count}
-          </div>
-        </div>
-        <div className="g-secondary-100 m-4 flex w-4/12 items-center justify-center rounded-md border-2 border-secondary-700 p-2 dark:bg-secondary-800">
-          <GoPeople className="mr-2 text-lg font-bold text-secondary-200" />
+        <div className="font-bold tracking-wider">{stars}</div>
+      </div>
+      <div className="m-4 flex w-4/12 items-center justify-center rounded-md border border-secondary-200 bg-secondary-100 p-2 dark:border-secondary-700 dark:bg-secondary-800">
+        <GoPeople className="mr-2 text-lg font-bold text-foreground" />
 
-          <div className="tracking-wider/widest font-bold">
-            {contributors?.length}
-          </div>
-        </div>
-      </>
+        <div className="font-bold tracking-wider">{contributors?.length}</div>
+      </div>
     </div>
   );
-}
-
-async function getData() {
-  const res = await octokit.paginate(
-    "GET /orgs/{org}/repos",
-    {
-      org: GITHUB_ORG,
-      per_page: 1000,
-    },
-    (response) => {
-      const data = response.data.map((ls) => {
-        return {
-          forks: ls.forks,
-          stargazers_count: ls.stargazers_count,
-        };
-      });
-      return data;
-    },
-  );
-
-  return res;
 }

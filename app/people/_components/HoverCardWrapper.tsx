590e1a17ee3c7c5a42efccfc2069dd46d180a128
Hridaya Pradhan <hridaya.pradhan123@gmail.com>
2024-04-06 19:18:58 +0545

    chore: removed useEffect from import in HoverCardWrapper

--- app/people/_components/HoverCardWrapper.tsx
+++ app/people/_components/HoverCardWrapper.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 const HoverInfocard = dynamic(() => import("./HoverInfocard"), {
   ssr: false,

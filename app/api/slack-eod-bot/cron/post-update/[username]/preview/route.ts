659df69a0e7df9049614279dac82020288d14e60
rithviknishad <mail@rithviknishad.dev>
2024-04-30 18:02:23 +0530

    Add scripts to support posting EOD Activities messages to Slack

--- app/api/slack-eod-bot/cron/post-update/[username]/preview/route.ts
+++ app/api/slack-eod-bot/cron/post-update/[username]/preview/route.ts
@@ -1,11 +1,10 @@
-import { NextRequest } from "next/server";
 import { EODUpdatesManager, sendSlackMessage } from "@/lib/slackbotutils";
 import { getContributorBySlug } from "@/lib/api";
 
 export const maxDuration = 300;
 
 export async function GET(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { username: string } },
 ) {
   const { username } = params;

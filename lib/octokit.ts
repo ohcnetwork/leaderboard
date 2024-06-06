95804ed987f82203404657b725a27376cc9f3dee
dhrumit parmar <dgparmar1406@gmail.com>
2024-06-06 11:24:11 +0530

    remove unwated changes

--- lib/octokit.ts
+++ lib/octokit.ts
@@ -1,12 +1,12 @@
 import "server-only";
-import { env } from "../env.mjs";
+import { env } from "@/env.mjs";
 import { Octokit } from "octokit";
 
 export const getGitHubAccessToken = () => {
   const accessToken = env.GITHUB_PAT as string | null;
 
   if (!accessToken) {
-    if (env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === "development") {
       console.warn("GITHUB_PAT is not configured in the environment.");
       return;
     }

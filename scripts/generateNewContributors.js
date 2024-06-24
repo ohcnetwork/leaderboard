8c626748d3ec289a74ad3165856c48f6b69d6b18
dhrumit parmar <dgparmar1406@gmail.com>
2024-06-24 11:57:15 +0530

    Fixing scraper-dry-run failing (Genrate markdown files)

--- scripts/generateNewContributors.js
+++ scripts/generateNewContributors.js
@@ -22,11 +22,11 @@ const basePath = join(process.env.DATA_REPO || process.cwd());
 function getNewContributors() {
   let newContributors = new Set();
 
-  fs.readdirSync(join(basePath, "data/github")).forEach((file) => {
+  fs.readdirSync(join(basePath, "data-repo/data/github")).forEach((file) => {
     newContributors.add(file.split(".")[0]);
   });
 
-  fs.readdirSync(join(basePath, "contributors")).forEach((file) => {
+  fs.readdirSync(join(basePath, "data-repo/contributors")).forEach((file) => {
     newContributors.delete(file.split(".")[0]);
   });
 

3eea484b0dad7759d224082a9b3a906ee00458c7
rithviknishad <mail@rithviknishad.dev>
2024-06-26 17:23:02 +0530

    add tests for validating github user data schema

---
+++ tests/github-data-schema.test.mjs
@@ -0,0 +1,30 @@
+import { expect, use } from "chai";
+import chaiJsonSchema from "chai-json-schema";
+import fs from "fs";
+import path, { join } from "path";
+import stripJsonComments from "strip-json-comments";
+import yaml from "yaml";
+
+const cwd = process.cwd();
+
+const SCHEMA_FILE = join(cwd, "schemas/github-data.yaml");
+const GH_DATA = join(cwd, process.env.DATA_REPO || "data-repo", "data/github");
+
+const schema = yaml.parse(fs.readFileSync(SCHEMA_FILE).toString());
+
+use(chaiJsonSchema);
+
+const filesInDir = fs
+  .readdirSync(GH_DATA)
+  .filter((file) => path.extname(file) === ".json");
+
+filesInDir.forEach((file) => {
+  const content = fs.readFileSync(join(GH_DATA, file)).toString();
+  const data = JSON.parse(stripJsonComments(content));
+
+  describe(`Validate '${file}'`, function () {
+    it("should be properly validated by the json schema", () => {
+      expect(data).to.be.jsonSchema(schema);
+    });
+  });
+});

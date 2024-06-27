4fd59d424f0e71762880f3f6fc750e2f9eb67ae2
dhrumit parmar <dgparmar1406@gmail.com>
2024-06-27 15:17:33 +0530

    Update test schema for discussion

--- tests/github-data-schema.test.mjs
+++ tests/github-data-schema.test.mjs
@@ -1,6 +1,7 @@
 import { expect, use } from "chai";
 import chaiJsonSchema from "chai-json-schema";
 import fs from "fs";
+import { describe, it } from "node:test";
 import path, { join } from "path";
 import stripJsonComments from "strip-json-comments";
 import yaml from "yaml";
@@ -10,13 +11,14 @@ const cwd = process.cwd();
 const SCHEMA_FILE = join(cwd, "schemas/github-data.yaml");
 const GH_DATA = join(cwd, process.env.DATA_REPO ?? "data-repo", "data/github");
 
-const schema = yaml.parse(fs.readFileSync(SCHEMA_FILE).toString());
+const schema = await yaml.parse(fs.readFileSync(SCHEMA_FILE).toString());
 
 use(chaiJsonSchema);
 
 const filesInDir = fs
   .readdirSync(GH_DATA)
   .filter((file) => path.extname(file) === ".json");
+console.log(filesInDir.length);
 
 filesInDir.forEach((file) => {
   const content = fs.readFileSync(join(GH_DATA, file)).toString();

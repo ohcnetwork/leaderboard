5cfe6c6cef4c4f45b1c5015f4b37d4aff9d5e5d1
rithviknishad <mail@rithviknishad.dev>
2024-07-19 13:00:18 +0530

    Add suspense boundary for discussions filter

--- app/discussions/layout.tsx
+++ app/discussions/layout.tsx
@@ -5,6 +5,7 @@ import { featureIsEnabled } from "@/lib/utils";
 import FilterDiscussions from "../../components/discussions/FilterDiscussions";
 import { categories } from "../../lib/discussion";
 import DiscussionLeaderboard from "../../components/discussions/DiscussionLeaderboard";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: `Disucssions | ${env.NEXT_PUBLIC_PAGE_TITLE}`,
@@ -21,7 +22,9 @@ export default function DiscussionsLayout({
     <div className="mx-auto max-w-6xl p-5">
       <div className="items-center gap-5 pb-8 lg:mt-10 lg:flex">
         <h1 className="text-3xl sm:text-4xl">Disucssions</h1>
-        <FilterDiscussions categories={categories} />
+        <Suspense fallback={<></>}>
+          <FilterDiscussions categories={categories} />
+        </Suspense>
       </div>
       <div className="flex w-full flex-col-reverse gap-3 lg:flex lg:flex-row">
         {children}

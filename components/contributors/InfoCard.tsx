f9d8ef987cabdefc69c3a820a53e70784055cc87
its-harshitgoel <its.harshitgoel@gmail.com>
2024-08-08 23:10:30 +0530

    Updated the icon

--- components/contributors/InfoCard.tsx
+++ components/contributors/InfoCard.tsx
@@ -20,7 +20,7 @@ export default async function InfoCard({
       className={clsx(
         "xl:text-left",
         isClickable &&
-        "rounded-lg border-2 border-transparent p-4 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary-400 hover:shadow-lg",
+          "rounded-lg border-2 border-transparent p-4 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary-400 hover:shadow-lg",
       )}
       role="listitem"
     >
@@ -106,7 +106,8 @@ export default async function InfoCard({
                       viewBox="0 0 1200 1227"
                       aria-hidden="true"
                     >
-                      <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />                    </svg>
+                      <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
+                    </svg>
                   </div>
                 </Link>
               </li>

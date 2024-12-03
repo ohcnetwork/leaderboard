b39a522c4ced07718ea1419bf52fbe4d0ae2aa8f
Srayash Singh <146334722+Srayash@users.noreply.github.com>
2024-12-03 14:21:49 +0530

    correct spelling for `Discussions`

--- app/discussions/layout.tsx
+++ app/discussions/layout.tsx
@@ -8,7 +8,7 @@ import DiscussionLeaderboard from "../../components/discussions/DiscussionLeader
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: `Disucssions | ${env.NEXT_PUBLIC_PAGE_TITLE}`,
+  title: `Discussions | ${env.NEXT_PUBLIC_PAGE_TITLE}`,
 };
 
 export default function DiscussionsLayout({

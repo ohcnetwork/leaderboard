ab8058fecf0765de743eaf32905048e6daeb6ddf
rithviknishad <mail@rithviknishad.dev>
2025-02-07 11:54:07 +0530

    setup vite project with react 19; tailwindcss v4

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});

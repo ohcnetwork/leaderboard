8989f760e1cb4495a7c24d107244038cbaffb7d7
rithviknishad <mail@rithviknishad.dev>
2025-10-28 11:39:49 +0530

    remove old code

--- app/feed/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingText from "@/components/LoadingText";
-
-export default function Loading() {
-  return <LoadingText text="Fetching latest events" />;
-}
--- app/leaderboard/[duration]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingText from "@/components/LoadingText";
-
-export default function Loading() {
-  return <LoadingText text="Ranking the contributors" />;
-}

313469dfc96088cbcabcb983f131543552bbff50
rithviknishad <mail@rithviknishad.dev>
2025-11-08 12:26:49 +0530

    add example config

--- scrapers/github/run.ts
+++ scrapers/github/run.ts
@@ -1,23 +1,5 @@
 import prepare from "@/scrapers/github/prepare";
-import { octokit } from "@/scrapers/github/utils/octokit";
 import { PGlite } from "@electric-sql/pglite";
-
-const requiredEventTypes = [
-  "IssueCommentEvent",
-  "IssuesEvent",
-  "PullRequestEvent",
-  "PullRequestReviewEvent",
-];
-
-const getOrgEvents = async (org: string) => {
-  const events = await octokit.paginate(
-    "GET /orgs/{org}/events",
-    { org },
-    (response) =>
-      response.data.filter((event) => requiredEventTypes.includes(event.type))
-  );
-  return events;
-};
 
 async function run(db: PGlite) {
   await prepare(db);

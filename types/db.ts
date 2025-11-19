045022e0aa824f5e8ec30e302b6a86779ea2a61c
rithviknishad <mail@rithviknishad.dev>
2025-11-20 01:04:02 +0530

    implement aggregates

--- types/db.ts
+++ types/db.ts
@@ -27,5 +27,43 @@ export interface Activity {
   link: string | null;
   text: string | null;
   points: number | null;
-  meta: Record<string, string> | null;
+  meta: Record<string, unknown> | null;
+}
+
+interface DurationAggregateValue {
+  type: "duration";
+  value: number;
+}
+
+interface NumberAggregateValue {
+  type: "number";
+  value: number;
+}
+
+interface StringAggregateValue {
+  type: "string";
+  value: string;
+}
+
+export type AggregateValue =
+  | DurationAggregateValue
+  | NumberAggregateValue
+  | StringAggregateValue;
+
+interface AggregateDefinitionBase {
+  slug: string;
+  name: string;
+  description: string | null;
+}
+
+export interface GlobalAggregate extends AggregateDefinitionBase {
+  value: AggregateValue | null;
+}
+
+export type ContributorAggregateDefinition = AggregateDefinitionBase;
+
+export interface ContributorAggregate {
+  aggregate: string; // FK to contributor_aggregate_definition.slug
+  contributor: string; // FK to contributor.username
+  value: AggregateValue;
 }

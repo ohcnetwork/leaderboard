68133b80503ad61e41ac90c764b06652061ded89
Sami Khan <samikhan15262822@gmail.com>
2026-04-05 03:45:49 +0500

    refact: make side bar sticky and have 100vh

--- apps/leaderboard-web/app/docs/[[...slug]]/layout.tsx
+++ apps/leaderboard-web/app/docs/[[...slug]]/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         enabled: false, // Disable Fumadocs nav since we have our own header
       }}
       containerProps={{ className: "my-16" }}
-      sidebar={{ className: "w-fit -my-16 pt-1" }}
+      sidebar={{ className: "w-fit -my-16 pt-1 sticky h-[100vh]" }}
       githubUrl="https://github.com/ohcnetwork/leaderboard"
     >
       {children}

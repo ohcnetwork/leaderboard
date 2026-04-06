803ead95920597bed8e0897d7ae66bfe4e242dfd
rithviknishad <mail@rithviknishad.dev>
2026-04-06 14:41:14 +0530

    fix footer margin for docs

--- apps/leaderboard-web/app/docs/[[...slug]]/layout.tsx
+++ apps/leaderboard-web/app/docs/[[...slug]]/layout.tsx
@@ -4,14 +4,16 @@ import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout
-      tree={source.pageTree}
-      nav={{
-        enabled: false, // Disable Fumadocs nav since we have our own header
-      }}
-      githubUrl="https://github.com/ohcnetwork/leaderboard"
-    >
-      {children}
-    </DocsLayout>
+    <div className="lb-docs-page">
+      <DocsLayout
+        tree={source.pageTree}
+        nav={{
+          enabled: false, // Disable Fumadocs nav since we have our own header
+        }}
+        githubUrl="https://github.com/ohcnetwork/leaderboard"
+      >
+        {children}
+      </DocsLayout>
+    </div>
   );
 }
